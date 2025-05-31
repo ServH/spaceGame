ed767ff9b179d27648936fc15d43f150e3bf969e
Alejandro <alejandro.camps@gmail.com>
2025-05-31 22:01:08 +0200

    📁 Mover archivos principales a src/config/ y src/core/

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,1 +1,64 @@
-// Move from js/utils.js - placeholder
+// Utility functions
+const Utils = {
+    // Calculate distance between two points
+    distance(a, b) {
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    },
+
+    // Random number between min and max
+    random(min, max) {
+        return Math.random() * (max - min) + min;
+    },
+
+    // Random integer between min and max
+    randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    // Clamp a value between min and max
+    clamp(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    },
+
+    // Linear interpolation
+    lerp(start, end, factor) {
+        return start + (end - start) * factor;
+    },
+
+    // Format numbers with K, M suffixes
+    formatNumber(num) {
+        if (num >= 1000000) {
+            return (num / 1000000).toFixed(1) + 'M';
+        } else if (num >= 1000) {
+            return (num / 1000).toFixed(1) + 'K';
+        }
+        return num.toString();
+    },
+
+    // Shuffle array
+    shuffle(array) {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    },
+
+    // Deep clone object
+    deepClone(obj) {
+        return JSON.parse(JSON.stringify(obj));
+    },
+
+    // Angle between two points
+    angle(a, b) {
+        return Math.atan2(b.y - a.y, b.x - a.x);
+    },
+
+    // Normalize angle to 0-2π
+    normalizeAngle(angle) {
+        while (angle < 0) angle += 2 * Math.PI;
+        while (angle >= 2 * Math.PI) angle -= 2 * Math.PI;
+        return angle;
+    }
+};
